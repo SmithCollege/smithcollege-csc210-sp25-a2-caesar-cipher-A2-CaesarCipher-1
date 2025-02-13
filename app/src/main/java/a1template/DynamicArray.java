b84cf9ea28b1a1d9eba0bc5aba9f1e6a5eecc626
Moya Zouhon <mzouhon@smith.edu>
2025-02-13 22:44:04 +0000

    created instance variables and prelim constructor for DA

--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -5,9 +5,19 @@ package a1template;
 public class DynamicArray<T> implements IndexAccess<T>{
     
     // What instance variables do you need?
+    char [] baseArray;
+    int offset;
+    int highIndex;
+    int lowIndex; 
 
     // Write a constructor to make a new DynamicArray Object from an array
+    public DynamicArray(char [] baseArray,int offset, int highIndex, int lowIndex ){
+        this.baseArray=baseArray;
+        this.offset=offset;
+        this.highIndex=highIndex;
+        this.lowIndex=lowIndex;
+    }
 
-    // Now implement the methods from IndexAccess?
+ }
+ // Now implement the methods from IndexAccess?
 
-}
