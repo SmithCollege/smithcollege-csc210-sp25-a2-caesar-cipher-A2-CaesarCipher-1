a41ebe7df2f260c4eeb935452d924c8a0254290e
Alyssa Kang <113084846+akang1217@users.noreply.github.com>
2025-02-16 19:34:26 +0000

    DynamicArray operations! We have to finish indexNum() to complete this

--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -5,9 +5,36 @@ package a1template;
 public class DynamicArray<T> implements IndexAccess<T>{
     
     // What instance variables do you need?
+    private T[] storage; //the underlying array
+    private int n; //the number of elements stored 
+    // private int capacity; //the total capacity of the array
 
     // Write a constructor to make a new DynamicArray Object from an array
 
     // Now implement the methods from IndexAccess?
+    public T get(int index){
+        indexNum(index);
+        return storage[index];
+    }
+
+    public T get(int index, int offset){
+        indexNum(index);
+        int x = 0;
+        if (index + offset >= n){
+            x = offset %n;
+        }
+        return storage[index+x];
+    }
+
+    public void set(int index, T element){
+        indexNum(index);
+        storage[index] = element;
+    }
+
+    public void indexNum(int index){
+        if (index < 0 || index >= n){
+            //throw?
+        }
+    }
 
 }
