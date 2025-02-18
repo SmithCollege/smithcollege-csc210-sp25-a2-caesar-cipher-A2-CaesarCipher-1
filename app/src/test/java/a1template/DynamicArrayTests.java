c010bd836481548269e1f5c70d9a5524346065bb
HM Rando <hrando@smith.edu>
2025-02-18 10:44:30 -0500

    fix typos

--- app/src/test/java/a1template/DynamicArrayTests.java
+++ app/src/test/java/a1template/DynamicArrayTests.java
@@ -40,7 +40,7 @@ public class DynamicArrayTests {
         createCount();
 
         DynamicArray<Integer> numberTest = new DynamicArray<Integer>(0, this.count);
-        assertEquals("Is a returned by the 0 index?", numberTest.get(0) == 0);
+        assertTrue("Is a returned by the 0 index?", numberTest.get(0) == 0);
         assertTrue("Is z returned by the 9th index?", numberTest.get(9) == 9);
     }
 
@@ -51,14 +51,14 @@ public class DynamicArrayTests {
 
         DynamicArray<Character> alphaTest = new DynamicArray<>(3, this.alphabet);
         System.out.println(alphaTest.get(0, 3));
-        assertEquals("Is d returned by the 0 index (offset 3)?",
-                alphaTest.get(0, 3).charValue(), 'd');
+        assertEquals("Is d returned by the 0 index (offset 3)?", 'd',
+                alphaTest.get(0, 3).charValue());
 
-        assertEquals("Is c returned by the -1 index (offset 3)?",
-                alphaTest.get(-1, 3).charValue(), 'c');
+        assertEquals("Is c returned by the -1 index (offset 3)?", 'c',
+                alphaTest.get(-1, 3).charValue());
 
-        assertEquals("Is a returned by the -3 index (offset 3)?",
-                alphaTest.get(-3, 3).charValue(), 'a');
+        assertEquals("Is a returned by the -3 index (offset 3)?", 'a',
+                alphaTest.get(-3, 3).charValue());
 
     }
 
