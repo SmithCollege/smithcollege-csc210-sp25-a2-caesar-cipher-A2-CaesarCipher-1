fed6b9b032bebb753845489bd1370768ab730856
HM Rando <hrando@smith.edu>
2025-02-22 08:07:35 -0500

    Change DynamicArray tests
    Remove offset from DynamicArray constructor

--- app/src/test/java/a1template/DynamicArrayTests.java
+++ app/src/test/java/a1template/DynamicArrayTests.java
@@ -29,7 +29,7 @@ public class DynamicArrayTests {
         // Create the base arrays that we will use to create DynamicArrays
         createAlphabet();
 
-        DynamicArray<Character> alphaTest = new DynamicArray<Character>(0, this.alphabet);
+        DynamicArray<Character> alphaTest = new DynamicArray<Character>(this.alphabet);
         assertTrue("Is a returned by the 0 index?", alphaTest.get(0) == 'a');
         assertTrue("Is z returned by the 25th index?", alphaTest.get(25) == 'z');
     }
@@ -39,7 +39,7 @@ public class DynamicArrayTests {
         // Create the base arrays that we will use to create DynamicArrays
         createCount();
 
-        DynamicArray<Integer> numberTest = new DynamicArray<Integer>(0, this.count);
+        DynamicArray<Integer> numberTest = new DynamicArray<Integer>(this.count);
         assertEquals("Is a returned by the 0 index?", numberTest.get(0) == 0);
         assertTrue("Is z returned by the 9th index?", numberTest.get(9) == 9);
     }
@@ -49,7 +49,7 @@ public class DynamicArrayTests {
         // Create the base arrays that we will use to create DynamicArrays
         createAlphabet();
 
-        DynamicArray<Character> alphaTest = new DynamicArray<>(3, this.alphabet);
+        DynamicArray<Character> alphaTest = new DynamicArray<>(this.alphabet);
         System.out.println(alphaTest.get(0, 3));
         assertEquals("Is d returned by the 0 index (offset 3)?",
                 alphaTest.get(0, 3).charValue(), 'd');
@@ -67,7 +67,7 @@ public class DynamicArrayTests {
         // Create the base arrays that we will use to create DynamicArrays
         createCount();
 
-        DynamicArray<Integer> numberTest = new DynamicArray<Integer>(5, this.count);
+        DynamicArray<Integer> numberTest = new DynamicArray<Integer>(this.count);
         assertTrue("Is 5 returned by the 0 index?", numberTest.get(0, 5) == 5);
         assertTrue("Is 0 returned by the -5 index?", numberTest.get(-5, 5) == 0);
     }
